8bcad1fbc1c0790894e9c4d8795889606bbc1aea
Piotr Justyna <piotr.justyna@gmail.com>
2025-08-19 19:58:14 +0100

    runtime image

---
+++ build-runtime-image.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+
+# 2024-12-04 PJ:
+# --------------
+# At the time of writing this, these directories are covered here:
+# https://cabal.readthedocs.io/en/latest/config.html#directories
+# Mounting them as volumes allows us to reuse cabal files
+# between containers saving a significant amount of build time.
+docker volume create wyvern-runtime-cabal-config
+docker volume create wyvern-runtime-cabal-cache
+docker volume create wyvern-runtime-cabal-state
+
+docker buildx build \
+  -t "wyvern-runtime:latest" \
+  -f "dockerfile-runtime" \
+  . \
+# && \
+# docker run \
+#   -it \
+#   -v "$HOME/.ssh/:/root/.ssh:ro" \
+#   -v "$(pwd):/root/code/wyvern" \
+#   -v wyvern-cabal-config:/root/.config/cabal \
+#   -v wyvern-cabal-cache:/root/.cache/cabal \
+#   -v wyvern-cabal-state:/root/.local/state/cabal \
+#   --rm "wyvern:latest"
