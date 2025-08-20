705ecf7c70ac3b7c73e24fe67097e234e44f6dbe
Piotr Justyna <piotr.justyna@gmail.com>
2025-08-20 21:43:36 +0100

    2 minimal runtime (#14)

--- build-runtime-image.sh
+++ build-runtime-image.sh
@@ -1,25 +1,12 @@
 #!/bin/sh
 
-# 2024-12-04 PJ:
-# --------------
-# At the time of writing this, these directories are covered here:
-# https://cabal.readthedocs.io/en/latest/config.html#directories
-# Mounting them as volumes allows us to reuse cabal files
-# between containers saving a significant amount of build time.
-docker volume create wyvern-runtime-cabal-config
-docker volume create wyvern-runtime-cabal-cache
-docker volume create wyvern-runtime-cabal-state
-
 docker buildx build \
   -t "wyvern-runtime:latest" \
   -f "dockerfile-runtime" \
   . \
-# && \
-# docker run \
-#   -it \
-#   -v "$HOME/.ssh/:/root/.ssh:ro" \
-#   -v "$(pwd):/root/code/wyvern" \
-#   -v wyvern-cabal-config:/root/.config/cabal \
-#   -v wyvern-cabal-cache:/root/.cache/cabal \
-#   -v wyvern-cabal-state:/root/.local/state/cabal \
-#   --rm "wyvern:latest"
+&& \
+docker run \
+  -v ./diagrams:/app/diagrams \
+  --rm wyvern-runtime:latest \
+  -i /app/diagrams/development-environment.txt \
+  -o /app/diagrams/development-environment.svg
