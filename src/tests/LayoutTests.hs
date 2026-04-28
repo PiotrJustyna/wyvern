af88b02f74b6d72fdb8a6eea73bc6362a33d7628
Piotr Justyna <piotr.justyna@gmail.com>
2026-04-28 20:38:08 +0100

    experiments with new connections

--- src/tests/LayoutTests.hs
+++ src/tests/LayoutTests.hs
@@ -14,11 +14,11 @@ specLayout1 = describe "layout1" $ do
     let block2 = Action (Just (ID "1")) "1"
     let block3 = Fork Nothing "-" [Action (Just (ID "2")) "2"] [Action (Just (ID "3")) "3"] Nothing
     let blocks = [block1, block2, block3]
-    let ([positionedBlock1, positionedBlock2, (PositionedFork _i _c [l3] [r3] _gCId x3 y3)], maxX, minY) = position' blocks 0.0 0.0
-    let (x1, y1) = getPosition positionedBlock1
-    let (x2, y2) = getPosition positionedBlock2
-    let (x3l, y3l) = getPosition l3
-    let (x3r, y3r) = getPosition r3
+    let ([positionedBlock1, positionedBlock2, (PositionedFork _i _c [l3] [r3] _gCId x3 y3 _maxX _minY)], skewerMaxX, skewerMinY) = position' blocks 0.0 0.0
+    let (x1, y1, maxX1, minY1) = getPosition positionedBlock1
+    let (x2, y2, maxX2, minY2) = getPosition positionedBlock2
+    let (x3l, y3l, maxX3l, minY3l) = getPosition l3
+    let (x3r, y3r, maxX3r, minY3r) = getPosition r3
 
     x1 `shouldBe` 0.0
     y1 `shouldBe` (defaultBoundingBoxHeight * (-3.0))
@@ -33,5 +33,5 @@ specLayout1 = describe "layout1" $ do
     x3r `shouldBe` 0.0 + defaultBoundingBoxWidth
     y3r `shouldBe` (defaultBoundingBoxHeight * (-1.0))
 
-    maxX `shouldBe` 6.0
-    minY `shouldBe` (defaultBoundingBoxHeight * (-4.0))
+    skewerMaxX `shouldBe` 6.0
+    skewerMinY `shouldBe` (defaultBoundingBoxHeight * (-4.0))
