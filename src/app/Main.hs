bdc964ce7aa4c872da6792981c5b80ed377027dd
Piotr Justyna <piotr.justyna@gmail.com>
2026-08-30 21:43:27 +0100

    experimental v3

--- src/app/Main.hs
+++ src/app/Main.hs
@@ -2,12 +2,15 @@ module Main where
 
 import Blocks (renderDiagram, reverse)
 import Constants (svgOptions)
+import Data.Map (lookup)
 import Diagrams.Backend.SVG (renderSVG')
+import ID
 import InputArguments (inputPath, outputPath, parseInput)
-import Layout (barebonesGamma, buildGammaConnection, connectionsV2, position, repositionBasedOnGamma)
+import Layout (barebonesGamma, buildGammaConnection, connectionsV2, position, repositionBasedOnGamma, repositionBasedOnGammaX, repositionOriginsBasedOnGamma)
 import Lexer (lexAll, runAlex)
 import Options.Applicative (execParser, fullDesc, header, helper, info, (<**>))
 import Parser (ParseResult (..), diagram)
+import PositionedBlock
 import PositionedBlock (extractGammaConnections, getMaxNumberOfShiftsPerDepth, qwe, toMap, toMapMicro)
 import Renderer (render, renderConnections)
 import Validator (validate)
@@ -32,32 +35,75 @@ main = do
             case validate blocks of
               Left validBlocks -> do
                 let positionedBlocks = position (Blocks.reverse validBlocks) 0.0 0.0
+                print positionedBlocks
+
+                -- V3 ->
+                let gamma = barebonesGamma positionedBlocks
+
+                putStrLn "barebones gamma:"
+                print gamma
+
+                -- let (gammaConnections, repositionedBlocks) =
+                --       foldr
+                --         ( \((xOrigin, yOrigin), gCId, maxXOrigin) (accuGammaConnections, accuPositionedBlocks) ->
+                --             let destinations = toMap accuPositionedBlocks
+                --                 repositionedBlocks = case Data.Map.lookup gCId destinations of
+                --                   Nothing -> accuPositionedBlocks
+                --                   Just (_x, y, maxX, _minY, _gammaShiftX, _gammaShiftY) -> repositionBasedOnGammaX (repositionBasedOnGamma accuPositionedBlocks [(y, 1)]) [(maxX, 1)]
+                --                 gammaConnection = fst $ buildGammaConnection gCId destinations xOrigin yOrigin maxXOrigin
+                --              in (gammaConnection <> accuGammaConnections, repositionedBlocks)
+                --         )
+                --         ([], positionedBlocks)
+                --         gamma
+
+                let repositionedBlocks = processGammaShifts gamma positionedBlocks
+
+                let gamma' = barebonesGamma repositionedBlocks
+
+                putStrLn "barebones gamma':"
+                print gamma'
+
+                let destinations' = toMap repositionedBlocks
+
+                let gammaConnections =
+                      foldr
+                        ( \((originX, originY), gCId, maxXOrigin) accu ->
+                            (fst $ buildGammaConnection gCId destinations' originX originY maxXOrigin) <> accu
+                        )
+                        []
+                        gamma'
+
+                let blockConnections3 = connectionsV2 repositionedBlocks
+                let renderedConnections3 = renderConnections $ blockConnections3 <> gammaConnections
+                -- let renderedConnections3 = renderConnections $ blockConnections3
+                -- <- V3
+
                 let destinationsMicro = toMapMicro positionedBlocks
                 -- TODO:
                 -- Probably a good idea to preserve those steps for debugging just in case.
                 let gammaConnections = extractGammaConnections destinationsMicro positionedBlocks
-                print gammaConnections
+                -- print gammaConnections
 
                 let defInput = qwe gammaConnections
-                print defInput
+                -- print defInput
 
                 let maxNumberOfShiftsPerDepth = getMaxNumberOfShiftsPerDepth defInput
-                print maxNumberOfShiftsPerDepth
+                -- print maxNumberOfShiftsPerDepth
 
-                let repositionedBlocks = repositionBasedOnGamma positionedBlocks maxNumberOfShiftsPerDepth
-                putStrLn "positionedBlocks:"
-                print positionedBlocks
-                putStrLn "repositionedBlocks:"
-                print repositionedBlocks
+                let repositionedBlocks' = repositionBasedOnGamma positionedBlocks maxNumberOfShiftsPerDepth
+                -- putStrLn "positionedBlocks:"
+                -- print positionedBlocks
+                -- putStrLn "repositionedBlocks':"
+                -- print repositionedBlocks'
 
-                let destinations = toMap repositionedBlocks
+                let destinations = toMap repositionedBlocks'
 
-                let blockConnections = connectionsV2 repositionedBlocks
-                let renderedConnections = renderConnections blockConnections
+                let blockConnections2 = connectionsV2 repositionedBlocks'
+                let renderedConnections2 = renderConnections blockConnections2
 
-                let gamma = barebonesGamma repositionedBlocks
-                putStrLn "barebones gamma:"
-                print gamma
+                let gamma2 = barebonesGamma repositionedBlocks'
+                -- putStrLn "barebones gamma:"
+                -- print gamma
 
                 let (gammaConnections', updatedDestinations) =
                       foldl
@@ -66,13 +112,16 @@ main = do
                              in (accuConnections <> newConnections, accuDestinations')
                         )
                         ([], destinations)
-                        gamma
+                        gamma2
 
                 -- print destinations
                 -- print updatedDestinations
 
+                -- rendering v3:
+                -- renderSVG' ((outputPath input) <> "_v3") svgOptions ((render repositionedBlocks) <> renderedConnections3)
+
                 -- rendering v2:
-                renderSVG' ((outputPath input) <> "_new") svgOptions ((render repositionedBlocks) <> renderConnections (blockConnections <> gammaConnections'))
+                -- renderSVG' ((outputPath input) <> "_new") svgOptions ((render repositionedBlocks) <> renderConnections (blockConnections2 <> gammaConnections'))
 
                 -- rendering v1:
                 renderSVG' (outputPath input) svgOptions (Blocks.renderDiagram validBlocks)
@@ -88,3 +137,18 @@ main = do
       info
         (parseInput <**> helper)
         (fullDesc <> header "Wyvern")
+
+processGammaShifts ::
+  [((Double, Double), ID, Double)] ->
+  [[PositionedBlock]] ->
+  [[PositionedBlock]]
+processGammaShifts [] positionedBlocks = positionedBlocks
+processGammaShifts (g@((xOrigin, yOrigin), gCId, maxXOrigin) : gs) positionedBlocks =
+  let destinations = toMap positionedBlocks
+      (repositionedBlocks, gs') = case Data.Map.lookup gCId destinations of
+        Nothing -> (positionedBlocks, gs)
+        Just (_x, y, maxX, _minY, _gammaShiftX, _gammaShiftY) ->
+          ( repositionBasedOnGammaX (repositionBasedOnGamma positionedBlocks [(y, 1)]) [(maxXOrigin, 1)],
+            repositionOriginsBasedOnGamma maxXOrigin y gs
+          )
+   in processGammaShifts gs' repositionedBlocks
--- src/lib/Layout.hs
+++ src/lib/Layout.hs
@@ -65,6 +65,21 @@ reposition'' b@(PositionedAction i c x y maxX minY) thresholdDepth numberOfShift
 reposition'' b@(PositionedHeadline i c x y maxX minY) thresholdDepth numberOfShifts = if (y <= thresholdDepth) then (PositionedHeadline i c x (y - repositionShift * (fromIntegral numberOfShifts)) maxX (minY - repositionShift * (fromIntegral numberOfShifts)), True) else (b, False)
 reposition'' b@(PositionedAddress i c x y maxX minY) thresholdDepth numberOfShifts = if (y <= thresholdDepth) then (PositionedAddress i c x (y - repositionShift * (fromIntegral numberOfShifts)) maxX (minY - repositionShift * (fromIntegral numberOfShifts)), True) else (b, False)
 
+repositionX'' :: PositionedBlock -> Double -> Int -> (PositionedBlock, Bool)
+repositionX'' b@(PositionedFork i c l r gCId x xR y maxX minYL minYR) thresholdWidth numberOfShifts =
+  let (l', lAnyRepositioned) = repositionX' l thresholdWidth numberOfShifts
+      (r', rAnyRepositioned) = repositionX' r thresholdWidth numberOfShifts
+      anyBranchRepositioned = lAnyRepositioned || rAnyRepositioned
+      shift = repositionShift * (fromIntegral numberOfShifts)
+   in if (x >= thresholdWidth)
+        then (PositionedFork i c l' r' gCId (x + shift) (xR + shift) y (maxX + shift) minYL minYR, True)
+        else (PositionedFork i c l' r' gCId x xR y (if anyBranchRepositioned then (maxX + shift) else maxX) minYL minYR, anyBranchRepositioned)
+repositionX'' b@(PositionedStartTerminator x y maxX minY) thresholdWidth numberOfShifts = if (x >= thresholdWidth) then (PositionedStartTerminator (x + repositionShift * (fromIntegral numberOfShifts)) y (maxX + repositionShift * (fromIntegral numberOfShifts)) minY, True) else (b, False)
+repositionX'' b@(PositionedEndTerminator x y maxX minY) thresholdWidth numberOfShifts = if (x >= thresholdWidth) then (PositionedEndTerminator (x + repositionShift * (fromIntegral numberOfShifts)) y (maxX + repositionShift * (fromIntegral numberOfShifts)) minY, True) else (b, False)
+repositionX'' b@(PositionedAction i c x y maxX minY) thresholdWidth numberOfShifts = if (x >= thresholdWidth) then (PositionedAction i c (x + repositionShift * (fromIntegral numberOfShifts)) y (maxX + repositionShift * (fromIntegral numberOfShifts)) minY, True) else (b, False)
+repositionX'' b@(PositionedHeadline i c x y maxX minY) thresholdWidth numberOfShifts = if (x >= thresholdWidth) then (PositionedHeadline i c (x + repositionShift * (fromIntegral numberOfShifts)) y (maxX + repositionShift * (fromIntegral numberOfShifts)) minY, True) else (b, False)
+repositionX'' b@(PositionedAddress i c x y maxX minY) thresholdWidth numberOfShifts = if (x >= thresholdWidth) then (PositionedAddress i c (x + repositionShift * (fromIntegral numberOfShifts)) y (maxX + repositionShift * (fromIntegral numberOfShifts)) minY, True) else (b, False)
+
 reposition' :: [PositionedBlock] -> Double -> Int -> ([PositionedBlock], Bool)
 reposition' bs y numberOfShifts =
   foldr
@@ -75,11 +90,31 @@ reposition' bs y numberOfShifts =
     ([], False)
     bs
 
+repositionX' :: [PositionedBlock] -> Double -> Int -> ([PositionedBlock], Bool)
+repositionX' bs x numberOfShifts =
+  foldr
+    ( \b (accuRepositionedBlocks, accuAnyRepositioned) ->
+        let (repositionedBlock, isRepositoned) = (repositionX'' b x numberOfShifts)
+         in (repositionedBlock : accuRepositionedBlocks, isRepositoned || accuAnyRepositioned)
+    )
+    ([], False)
+    bs
+
 reposition :: [[PositionedBlock]] -> Double -> Int -> ([[PositionedBlock]], Bool)
 reposition ss y numberOfShifts =
   foldr
     ( \s (accuRepositionedSkewers, accuAnyRepositioned) ->
         let (repositionedSkewer, isRepositioned) = (reposition' s y numberOfShifts)
+         in (repositionedSkewer : accuRepositionedSkewers, isRepositioned || accuAnyRepositioned)
+    )
+    ([], False)
+    ss
+
+repositionX :: [[PositionedBlock]] -> Double -> Int -> ([[PositionedBlock]], Bool)
+repositionX ss x numberOfShifts =
+  foldr
+    ( \s (accuRepositionedSkewers, accuAnyRepositioned) ->
+        let (repositionedSkewer, isRepositioned) = (repositionX' s x numberOfShifts)
          in (repositionedSkewer : accuRepositionedSkewers, isRepositioned || accuAnyRepositioned)
     )
     ([], False)
@@ -105,86 +140,8 @@ buildGammaConnection gCId destinations x y maxX =
     Nothing -> error $ "gamma connection id \"" <> show gCId <> "\" does not exist in the collection of block identifiers: " <> show destinations
     (Just destination) -> (buildGammaConnection' x y maxX destination, Data.Map.adjust (\(vX, vY, vMaxX, vMinY, vGammaShiftX, vGammaShiftY) -> (vX, vY, vMaxX, vMinY, vGammaShiftX + repositionShift, vGammaShiftY + repositionShift)) gCId destinations)
 
--- connections'' :: PositionedBlock -> Map ID (Double, Double, Double, Double, Double, Double) -> ([((Double, Double), (Double, Double))], Map ID (Double, Double, Double, Double, Double, Double))
--- connections'' (PositionedFork _i _c l r gCId x y maxX minYL minYR) destinations =
---   let minY = min minYL minYR
---       (lc, lDestinations) = case l of
---         [] -> ([((x, y), (x, minY - defaultBoundingBoxHeight * 0.25))], destinations)
---         bs@(b : _) ->
---           let (lx, ly, _lmaxX, _lMinY) = getPosition b
---            in case last bs of
---                 (PositionedFork _i _c _l _r _gCId _x _y _maxX _minYL _minYR) -> ([((x, y), (lx, ly))], destinations)
---                 lastB ->
---                   let (lastx, lasty, _lastmaxX, _lastMinY) = getPosition lastB
---                    in ([((x, y), (lx, ly)), ((lastx, lasty), (x, minY - defaultBoundingBoxHeight * 0.25))], destinations)
---       (rc, rDestinations) = case gCId of
---         Nothing -> case r of
---           [] -> ([((x, y), (maxX - defaultBoundingBoxWidth * 0.5, y)), ((maxX - defaultBoundingBoxWidth * 0.5, y), (maxX - defaultBoundingBoxWidth * 0.5, minY)), ((maxX - defaultBoundingBoxWidth * 0.5, minY), (x, minY))], lDestinations)
---           bs@(b : _) ->
---             let (rx, ry, _rmaxX, _rMinY) = getPosition b
---              in case last bs of
---                   (PositionedFork _i _c _l _r _gCId fx fy _maxX _minYL _minYR) -> ([((x, y), (rx, y)), ((rx, y), (rx, ry)), ((rx, minY), (x, minY)), ((fx, fy), (fx, minY))], lDestinations) -- TODO: ((rx, minY), (x, minY)) a mistake? ((fx, fy), (fx, minY)) - also a mistake and can lead to clashes
---                   lastB ->
---                     let (lastx, lasty, _lastmaxX, _lastMinY) = getPosition lastB
---                      in ([((x, y), (rx, y)), ((rx, y), (rx, ry)), ((lastx, lasty), (lastx, minY)), ((lastx, minY), (x, minY))], lDestinations)
---         (Just gCId') -> case r of
---           [] -> buildGammaConnection gCId' lDestinations x y maxX
---           bs@(b : _) ->
---             let (rx, ry, _rmaxX, _rMinY) = getPosition b
---              in case last bs of
---                   (PositionedFork _i _c _l _r _gCId fx fy fmaxX fminY) ->
---                     let (gammaConnections, lDestinations') = buildGammaConnection gCId' lDestinations fx fminY fmaxX
---                      in ([((x, y), (rx, y)), ((rx, y), (rx, ry))] <> gammaConnections, lDestinations')
---                   lastB ->
---                     let (lastx, lasty, lastmaxX, _lastMinY) = getPosition lastB
---                         lasty' = lasty - defaultBoundingBoxHeight * 0.5
---                         (gammaConnections, lDestinations') = buildGammaConnection gCId' lDestinations lastx lasty' lastmaxX
---                      in ([((x, y), (rx, y)), ((rx, y), (rx, ry)), ((lastx, lasty), (lastx, lasty'))] <> gammaConnections, lDestinations') -- TODO: there are two connections here: the leading one and the gamma one
---       (lc', lDestinations') = connections' l rDestinations
---       -- 2026-07-29 PJ:
---       -- ==============
---       -- The section below (lc'') adds an extra line connecting the left branch with the end of the fork.
---       -- We need that extra line when the right branch is longer than the left one.
---       -- Without it, in such scenarios, there would be a gap between the end of the fork and the last left branch's block.
---       lastLeftPosition@(lLPX, lLPY) = snd $ last lc'
---       lc'' = lc' <> (if lLPY > minY + defaultBoundingBoxHeight then [((lLPX, lLPY - defaultBoundingBoxHeight), (x, minY))] else [])
---       (rc', rDestinations') = connections' r lDestinations'
---    in (lc <> rc <> lc' <> lc'' <> rc', rDestinations')
--- connections'' _ destinations = ([], destinations)
-
--- connections' :: [PositionedBlock] -> Map ID (Double, Double, Double, Double, Double, Double) -> ([((Double, Double), (Double, Double))], Map ID (Double, Double, Double, Double, Double, Double))
--- connections' [] destinations = ([], destinations)
--- connections' [pB] destinations = connections'' pB destinations
--- connections' (pB1 : pB2 : pBs) destinations =
---   case pB1 of
---     (PositionedFork _i _c l _r _gCId x1 y1 maxX1 minY1) ->
---       let position2@(x2, y2, maxX2, minY2) = getPosition pB2
---           lConnection = case l of
---             [] -> []
---             _ -> [((x1, minY1), (x2, y2))]
---           (firstConnections, firstDestinations) = connections'' pB1 destinations
---           (remainingConnections, remainingDestinations) = connections' (pB2 : pBs) firstDestinations
---        in (lConnection <> firstConnections <> remainingConnections, remainingDestinations)
---     _ ->
---       let position1@(x1, y1, maxX1, minY1) = getPosition pB1
---           position2@(x2, y2, maxX2, minY2) = getPosition pB2
---           connection = [((x1, y1), (x2, y2))]
---           (remainingConnections, remainingDestinations) = connections' (pB2 : pBs) destinations
---        in (connection <> remainingConnections, remainingDestinations)
-
--- connections :: [[PositionedBlock]] -> Map ID (Double, Double, Double, Double, Double, Double) -> [((Double, Double), (Double, Double))]
--- connections positionedBlocks destinations =
---   fst $
---     foldr
---       ( \pBs (accuConnections, accuDestinations) ->
---           let (newConnections, newDestinations) = connections' pBs destinations
---            in (newConnections <> accuConnections, newDestinations)
---       )
---       ([], destinations)
---       positionedBlocks
-
 connectionsV2'' :: PositionedBlock -> [((Double, Double), (Double, Double))]
-connectionsV2'' (PositionedFork _i _c l r gCId x _xR y maxX minYL minYR) =
+connectionsV2'' (PositionedFork _i _c l r gCId x xR y maxX minYL minYR) =
   let minY = min minYL minYR
       lc = case l of
         [] -> [((x, y), (x, minY - defaultBoundingBoxHeight * 0.25))]
@@ -197,7 +154,7 @@ connectionsV2'' (PositionedFork _i _c l r gCId x _xR y maxX minYL minYR) =
                    in [((x, y), (lx, ly)), ((lastx, lasty), (x, minY))]
       rc = case r of
         [] -> case gCId of
-          Nothing -> [((x, y), (maxX - defaultBoundingBoxWidth * 0.5, y)), ((maxX - defaultBoundingBoxWidth * 0.5, y), (maxX - defaultBoundingBoxWidth * 0.5, minY)), ((maxX - defaultBoundingBoxWidth * 0.5, minY), (x, minY))]
+          Nothing -> [((x, y), (maxX, y)), ((maxX, y), (maxX, minY)), ((maxX, minY), (x, minY))]
           _ -> []
         bs@(b : _) ->
           let (rx, ry, _rmaxX, _rMinY) = getPosition b
@@ -223,7 +180,12 @@ connectionsV2'' (PositionedFork _i _c l r gCId x _xR y maxX minYL minYR) =
           let (lLPX, lLPY) = snd $ last lc'
            in if lLPY > minY + defaultBoundingBoxHeight then [((lLPX, lLPY - defaultBoundingBoxHeight * 0.5), (x, minY))] else []
       rc' = connectionsV2' r
-   in lc <> rc <> lc' <> rc' <> lc''
+      rc'' = case rc' of
+        [] -> []
+        _ ->
+          let (rLPX, rLPY) = snd $ last rc'
+           in if rLPY > minY + defaultBoundingBoxHeight then [((rLPX, rLPY - defaultBoundingBoxHeight * 0.5), (rLPX, minY))] else []
+   in lc <> rc <> lc' <> rc' <> lc'' <> rc''
 connectionsV2'' _ = []
 
 connectionsV2' :: [PositionedBlock] -> [((Double, Double), (Double, Double))]
@@ -259,7 +221,7 @@ barebonesGamma'' (PositionedFork _i _c l r gCId x xR y maxX minYL minYR) =
         (Just gCId') -> case r of
           [] -> [((x, y), gCId', maxX)]
           _ -> [((xR, minYR), gCId', maxX)]
-   in gamma <> lGamma <> rGamma
+   in rGamma <> lGamma <> gamma
 barebonesGamma'' _ = []
 
 barebonesGamma' :: [PositionedBlock] -> [((Double, Double), ID, Double)]
@@ -268,5 +230,19 @@ barebonesGamma' = foldr (\pB accuGamma -> accuGamma <> barebonesGamma'' pB) []
 barebonesGamma :: [[PositionedBlock]] -> [((Double, Double), ID, Double)]
 barebonesGamma = foldr (\pBs accuGamma -> accuGamma <> barebonesGamma' pBs) []
 
+-- 2026-08-25 PJ:
+-- ==============
+-- TODO: simplify if V3 is the right approach.
 repositionBasedOnGamma :: [[PositionedBlock]] -> [(Double, Int)] -> [[PositionedBlock]]
 repositionBasedOnGamma positionedBlocks repositionInstructions = foldr (\(thresholdDepth, numberOfShifts) accu -> fst $ reposition accu thresholdDepth numberOfShifts) positionedBlocks repositionInstructions
+
+-- 2026-08-25 PJ:
+-- ==============
+-- TODO: simplify if V3 is the right approach.
+repositionBasedOnGammaX :: [[PositionedBlock]] -> [(Double, Int)] -> [[PositionedBlock]]
+repositionBasedOnGammaX positionedBlocks repositionInstructions = foldr (\(thresholdWidth, numberOfShifts) accu -> fst $ repositionX accu thresholdWidth numberOfShifts) positionedBlocks repositionInstructions
+
+repositionOriginsBasedOnGamma :: Double -> Double -> [((Double, Double), ID, Double)] -> [((Double, Double), ID, Double)]
+repositionOriginsBasedOnGamma _repositionInstructionsX _repositionInstructionsY [] = []
+repositionOriginsBasedOnGamma repositionInstructionsX repositionInstructionsY (o@((originX, originY), _gCId, _maxXOrigin) : os) =
+  ((if originX >= repositionInstructionsX then originX + repositionShift else originX, if originY <= repositionInstructionsY then originY - repositionShift else originY), _gCId, _maxXOrigin) : repositionOriginsBasedOnGamma repositionInstructionsX repositionInstructionsY os
